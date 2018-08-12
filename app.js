2ea75deb9c454b37912e9d9756d4561f1f23999f
Thea <tmush2014@agruni.edu.ge>
2018-08-12 21:58:03 +0400

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e){
+
+  const amount = document.getElementById('amount');
+  e.preventDefault();
+}
